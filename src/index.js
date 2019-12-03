a1f2de02354660d21b0a8631876215f4923cb920
tedilesmana <tedi.lesmana0123@gmail.com>
2019-12-03 11:26:55 +0700

    setting awal react js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import { HashRouter } from "react-router-dom";
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+	<HashRouter basename="/" > 
+		<App />
+	</HashRouter>
+	, document.getElementById('root'));
+
 serviceWorker.unregister();
+
+
